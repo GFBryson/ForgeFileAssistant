e9bc8181bab9e9c15d962d8098e55b068baff112
GFBryson <gfb@gillianfbryson.ca>
2021-12-06 23:49:22 -0800

    Adding in support for having file browsers start from their last location for easier file finding

--- src/get_paths.py
+++ src/get_paths.py
@@ -1,12 +1,19 @@
 from tkinter import filedialog, StringVar
 
-def get_filepath(path_var: StringVar, do_after=None, filetypes: tuple = [("All filetypes", "*.*")]):
-    path_var.set(filedialog.askopenfilename(filetypes=filetypes))
+
+def get_filepath(path_var: StringVar, do_after=None, filetypes: list = [("All filetypes", "*.*")]):
+    print(path_var.get())
+    initialdir = None if path_var.get() == "" else path_var.get()
+    new_path = filedialog.askopenfilename(filetypes=filetypes, initialdir=initialdir)
+    path_var.set( initialdir if new_path == "" else new_path)
     if do_after is not None:
         do_after()
 
 
 def get_directorypath(path_var: StringVar, do_after=None):
-    path_var.set(filedialog.askdirectory())
+    print(f"initial_dir:{path_var.get()}")
+    initialdir = None if path_var.get() == "" else path_var.get()
+    new_dir = filedialog.askdirectory(initialdir=initialdir)
+    path_var.set(initialdir if new_dir == "" else new_dir)
     if do_after is not None:
         do_after()
