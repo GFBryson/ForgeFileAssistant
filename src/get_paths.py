07019f3f5b3abf4da3840a8d3223e6bdf062a1a1
GFBryson <gfb@gillianfbryson.ca>
2021-12-10 00:47:32 -0800

    changed type hint of filetypes paramater to list from tuple

--- src/get_paths.py
+++ src/get_paths.py
@@ -1,6 +1,6 @@
 from tkinter import filedialog, StringVar
 
-def get_filepath(path_var: StringVar, do_after=None, filetypes: tuple = [("All filetypes", "*.*")]):
+def get_filepath(path_var: StringVar, do_after=None, filetypes: list = [("All filetypes", "*.*")]):
     path_var.set(filedialog.askopenfilename(filetypes=filetypes))
     if do_after is not None:
         do_after()
