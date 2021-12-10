25eb8962b0e48c09e37504b6a2494ebcd593ca23
GFBryson <gfb@gillianfbryson.ca>
2021-12-10 00:51:01 -0800

    use common get_image_file_picker_frame

--- src/optimize_image.py
+++ src/optimize_image.py
@@ -3,6 +3,7 @@ from tkinter import *
 
 from PIL import Image
 
+from src.common_frames import get_image_file_picker_frame
 from src.get_paths import get_filepath
 
 Image.MAX_IMAGE_PIXELS = None
@@ -17,15 +18,8 @@ def save_image_as_webp(image_path: str):
 def get_image_optimization_frame(window):
     tab_root = Frame(window)
 
-    source_frame = Frame(tab_root)
-    image_filepath_str = StringVar()
-    image_filepath_label = Label(source_frame, text='Source Image for Optimization').pack(side='left')
-    json_path = Entry(source_frame, textvariable=image_filepath_str).pack(side='left', expand=True, fill='x')
-
-    browse_button_1 = Button(source_frame, text="Browse", command=lambda: get_filepath(image_filepath_str, filetypes=[("Image File", "*.png *.jpeg *.jpg")])).pack(
-        side='right')
-
-    source_frame.pack(fill='x', side='top')
+    source_image_frame, image_filepath_str = get_image_file_picker_frame(tab_root)
+    source_image_frame.pack(fill='x', side='top')
 
     # ------------
     bottom_frame = Frame(tab_root)
